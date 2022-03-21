4ce95bbe403d4818be2d2aa91335e634bcec7628
radenadhyaksa <raksa69@gmail.com>
2022-03-21 14:40:45 +0700

    update widget - container.dart

--- lib/widget/container.dart
+++ lib/widget/container.dart
@@ -9,7 +9,7 @@ void main(List<String> args) {
   );
 }
 
-///Width & Height
+///Width & Height :
 ///
 // Container(
 //   child: Text('Hi', style: TextStyle(fontSize: 40),),
@@ -18,11 +18,66 @@ void main(List<String> args) {
 //   height: 100,
 // )
 
-///Padding & Margin
+///Padding & Margin :
 ///
 //Container(
 //  child: Text('Hi', style: TextStyle(fontSize: 40),),
 //  color: Colors.blue,
 //  padding: EdgeInsets.all(10),
 // )
+
+///color :
+///
+// Container(
+//   child: Text('Hi', style: TextStyle(fontSize: 40),),
+//   decoration: BoxDecoration(
+//     color: Colors.red,
+//   ),
+// )
+
+
+///shape :
+///
+// Container(
+//   child: Text('Hi', style: TextStyle(fontSize: 40),),
+//   decoration: BoxDecoration(
+//     color: Colors.red,
+//     shape: BoxShape.circle,
+//   ),
+// )
+
+
+/**
+ * Shadow :
+Di dalamnya terdapat BoxShadow yang artinya 
+pada Container kita dapat memberikan banyak bayangan atau shadow.
+ */
+// Container(
+//   child: Text('Hi', style: TextStyle(fontSize: 40)),
+//   decoration: BoxDecoration(
+//     color: Colors.red,
+//     boxShadow: [
+//       BoxShadow(
+//         color: Colors.black,
+//         offset: Offset(3, 6),
+//         blurRadius: 10,
+//       ),
+//     ],
+//   ),
+// )
+
+
+/**
+ * Border :
+ * Border merupakan batas garis dengan content (child). 
+ */
+// Container(
+//   child: Text('Hi', style: TextStyle(fontSize: 40),),
+//   decoration: BoxDecoration(
+//   color: Colors.red,
+//   border: Border.all(color: Colors.green, width: 3),
+//   borderRadius: BorderRadius.circular(10),
+//   ),
+// )
+
 
