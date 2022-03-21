d3792d102598fbe2c1b4002921dcb6180911709a
radenadhyaksa <raksa69@gmail.com>
2022-03-21 14:33:20 +0700

    add widget - container.dart

---
+++ lib/widget/container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  //pengenalan container
+  Container(
+    child: Text('Hi', style: TextStyle(fontSize: 40)),
+    color: Colors.blue,
+  );
+}
+
+///Width & Height
+///
+// Container(
+//   child: Text('Hi', style: TextStyle(fontSize: 40),),
+//   color: Colors.blue,
+//   width: 200,
+//   height: 100,
+// )
+
+///Padding & Margin
+///
+//Container(
+//  child: Text('Hi', style: TextStyle(fontSize: 40),),
+//  color: Colors.blue,
+//  padding: EdgeInsets.all(10),
+// )
+
