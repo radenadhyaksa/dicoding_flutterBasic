c11cf1ba98b94cadb84afa74df8e81ac44cb116a
radenadhyaksa <raksa69@gmail.com>
2022-03-21 14:49:22 +0700

    add widget - row and column.dart

---
+++ lib/widget/row_column.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/**
+ *  widget yang kita gunakan berjajar secara vertikal atau horizonta
+ * widget Row :
+ * widget Row merupakan suatu widget yang digunakan untuk 
+ * membuat widget-widget tersusun berjajar secara horizontal.
+ */
+
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: <Widget> [
+    Icon(Icons.share),
+    Icon(Icons.thumb_up),
+    Icon(Icons.thumb_down),
+  ],
+)
+
+
+/**
+ * Widget Column
+ * Kebalikan dari Row, 
+ * Column merupakan suatu widget yang digunakan 
+ * untuk membuat widget-widget tersusun berjajar secara vertikal.
+ */
+
+Column(
+  children: <Widget>[
+    Text(
+      'Sebuah Judul',
+      style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+    ),
+    Text('Lorem ipsum dolor sit amet'),
+  ],
+)
